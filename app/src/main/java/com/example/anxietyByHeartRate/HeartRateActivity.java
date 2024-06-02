06dc7305feaf2c7fa9a173e6918f714162002885
shiradahan5 <shiradahan590@gmail.com>
2024-06-02 22:59:20 +0300

    add Shira's user

--- app/src/main/java/com/example/anxietyByHeartRate/HeartRateActivity.java
+++ app/src/main/java/com/example/anxietyByHeartRate/HeartRateActivity.java
@@ -11,12 +11,14 @@ import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.ActionMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -34,8 +36,16 @@ import java.util.LinkedList;
 public class HeartRateActivity extends AppCompatActivity {
 
     private static final int REQUEST_NOTIFICATION_PERMISSION = 1;
+    private static final int REQUEST_PERMISSIONS_CODE = 100;
+    private static final String[] REQUIRED_PERMISSIONS = {
+            Manifest.permission.FOREGROUND_SERVICE_HEALTH,
+            Manifest.permission.ACTIVITY_RECOGNITION,
+            Manifest.permission.BODY_SENSORS,
+            Manifest.permission.HIGH_SAMPLING_RATE_SENSORS
+    };
+
     private TextView heartRateTextView;
-    private int heartRate = 60; // Initial heart rate value (beats per minute)
+    private int heartRate = 60;
     private Handler handler = new Handler();
     private FirebaseFirestore db;
     private LinkedList<HeartRateStamp> HeartRateData;
@@ -44,6 +54,12 @@ public class HeartRateActivity extends AppCompatActivity {
     private VideoView videoView;
     private ConstraintLayout constraintLayout;
 
+    @Nullable
+    @Override
+    public ActionMode startActionMode(ActionMode.Callback callback) {
+        return super.startActionMode(callback);
+    }
+
     @SuppressLint("UnspecifiedRegisterReceiverFlag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,11 +67,10 @@ public class HeartRateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_heart_rate);
         db = FirebaseFirestore.getInstance();
 
-
         heartRateTextView = findViewById(R.id.heartRateTextView);
         startStopButton = findViewById(R.id.startStopButton);
         videoView = findViewById(R.id.videoView);
-        constraintLayout = findViewById(R.id.constraintLayout); // Reference the ConstraintLayout
+        constraintLayout = findViewById(R.id.constraintLayout);
 
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.heart_rate_video_2);
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -67,25 +82,34 @@ public class HeartRateActivity extends AppCompatActivity {
             }
         });
 
-        // Check notification permission for Android Tiramisu (API level 33) and above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.POST_NOTIFICATIONS},
-                        REQUEST_NOTIFICATION_PERMISSION);
+        // Check and request necessary permissions
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_PERMISSIONS_CODE);
+        } else {
+            initializeHeartRateService();
+        }
+    }
+
+    // Check if all required permissions are granted
+    private boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
             }
         }
+        return true;
+    }
 
+    // Initialize the heart rate service and other components
+    private void initializeHeartRateService() {
         try {
-            readHeartRateData(); // Assuming the file name is data.txt
+            readHeartRateData();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         heartRate = HeartRateData.remove(HeartRateData.size() - 1).beatsPerMinute;
 
-        // Button click listener to start/stop the service
         startStopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,13 +121,39 @@ public class HeartRateActivity extends AppCompatActivity {
             }
         });
 
-        // Set initial text for heartRateTextView
         setInitialHeartRateTextView();
 
-        // Register broadcast receiver to receive heart rate updates
         IntentFilter filter = new IntentFilter();
         filter.addAction("HEART_RATE_UPDATE");
-        registerReceiver(heartRateReceiver, filter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(heartRateReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            registerReceiver(heartRateReceiver, filter);
+        }
+    }
+
+    // Handle the permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            if (grantResults.length > 0 && areAllPermissionsGranted(grantResults)) {
+                initializeHeartRateService();
+            } else {
+                // Handle the case where the user denies the permission
+                // You can show a message or disable certain features
+            }
+        }
+    }
+
+    // Check if all permissions are granted
+    private boolean areAllPermissionsGranted(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private void setInitialHeartRateTextView() {
@@ -115,14 +165,12 @@ public class HeartRateActivity extends AppCompatActivity {
     }
 
     private void startHeartRateService() {
-        Intent serviceIntent = new Intent(this, HeartRateService.class);;
+        Intent serviceIntent = new Intent(this, HeartRateService.class);
         startService(serviceIntent);
         isServiceRunning = true;
         startStopButton.setText("Stop");
         heartRateTextView.setText("Heart Rate: " + heartRate + " BPM");
 
-
-        // Move the button to the bottom of the screen
         moveButtonToBottom();
 
         videoView.setVisibility(View.VISIBLE);
@@ -135,14 +183,12 @@ public class HeartRateActivity extends AppCompatActivity {
         isServiceRunning = false;
         startStopButton.setText("Start");
 
-        // Move the button back to the center of the screen
         moveButtonToCenter();
 
         videoView.stopPlayback();
-        videoView.resume(); // Reset the video to the start position
+        videoView.resume();
         videoView.setVisibility(View.GONE);
 
-        // Set text for heartRateTextView
         setInitialHeartRateTextView();
     }
 
@@ -150,10 +196,9 @@ public class HeartRateActivity extends AppCompatActivity {
         ConstraintSet constraintSet = new ConstraintSet();
         constraintSet.clone(constraintLayout);
         constraintSet.clear(R.id.startStopButton, ConstraintSet.TOP);
-        constraintSet.connect(R.id.startStopButton, ConstraintSet.TOP, R.id.heartRateTextView, ConstraintSet.BOTTOM); // Move 20dp below the text view
+        constraintSet.connect(R.id.startStopButton, ConstraintSet.TOP, R.id.heartRateTextView, ConstraintSet.BOTTOM);
         constraintSet.applyTo(constraintLayout);
     }
-
 
     private void moveButtonToCenter() {
         ConstraintSet constraintSet = new ConstraintSet();
@@ -165,39 +210,20 @@ public class HeartRateActivity extends AppCompatActivity {
         constraintSet.applyTo(constraintLayout);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_NOTIFICATION_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startHeartRateService();
-            }  // Handle the case where the user denies the permission
-
-        }
-    }
-
     private void readHeartRateData() throws IOException {
         HeartRateData = new LinkedList<>();
         InputStream is = this.getResources().openRawResource(R.raw.data);
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         String line;
-        // Skip the header
         reader.readLine();
         while ((line = reader.readLine()) != null) {
-            // Split the line by tab character
             String[] parts = line.split(",");
-            // Extract the beats per minute value (assuming it's the last column)
             int beatsPerMinute = Integer.parseInt(parts[parts.length - 1]);
             String time = parts[parts.length - 2];
-            // Push the beats per minute value onto the stack
             HeartRateData.push(new HeartRateStamp(time, beatsPerMinute));
         }
         reader.close();
     }
-
-
 
     private BroadcastReceiver heartRateReceiver = new BroadcastReceiver() {
         @Override
@@ -217,11 +243,7 @@ public class HeartRateActivity extends AppCompatActivity {
         if (isServiceRunning) {
             startStopButton.setText("Stop");
             heartRateTextView.setText("Heart Rate: " + heartRate + " BPM");
-
-
-            // Move the button to the bottom of the screen
             moveButtonToBottom();
-
             videoView.setVisibility(View.VISIBLE);
             videoView.start();
         } else {
